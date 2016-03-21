9820dc4688bf5026e0980f6991aac1cf60bac5c5
Saleh Saeed <saleh.saiid@gmail.com>
2016-03-21 16:41:52 +0200

    Image manipulation server

---
+++ Config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: saleh
+ * Date: 3/21/16
+ * Time: 3:49 PM
+ */
+
+return [
+    's3'    => [
+        /* Source filesystem */
+        'source' => [
+            'credentials'   => [
+                'key'   => '',
+                'secret'    => ''
+            ],
+            'region'    => '',
+            'version'   => 'latest',
+            'bucket'    => ''
+        ],
+
+        /* Cache filesystem */
+        'cache' => [
+            'credentials'   => [
+                'key'   => '',
+                'secret'    => ''
+            ],
+            'region'    => '',
+            'version'   => 'latest',
+            'bucket'    => ''
+        ]
+    ],
+
+    'base_url'              => '',
+    'source_path_prefix'    => '',
+    'cache_path_prefix'     => '',
+    'group_cache_in_folders'    => true,
+    'signkey'               => 'lPSJjVjm//Ts2715OjAJ+pUPRX5gc2bZM7MRfW8BD+Ds18lTlnlEW++u2xZY2lLssBtxkg7ehiy3oi9iOUH75NDU/6IX3O9/XmFVI47j+eKIhCXP1QO2C9T1UwGzw82ejjtz627fP0bCEKEstasmEASoxYZkdWzuuo4RAZsnDBE='
+];
