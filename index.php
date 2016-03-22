1e31571faf9116657f0793e5877219eabe51233f
Saleh Saeed <saleh.saiid@gmail.com>
2016-03-22 12:38:56 +0200

    Implement check Signature before display image

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <?php
+use League\Glide\Signatures\SignatureFactory;
+
 require 'vendor/autoload.php';
 
 $server = require 'server.php';
 
+$config = require 'Config.php';
+
 $router = new AltoRouter();
 
+$router->map('GET', '/[*:path]', function ($path) use ($server, $config) {
+    try {
+        // Validate HTTP signature
+        SignatureFactory::create($config['signkey'])->validateRequest($path, $_GET);
 
-$router->map('GET', '/[*:path]', function($path) use ($server){
-    try{
         echo $server->outputImage($path, $_GET);
-    }catch(Exception $e){
-        header( $_SERVER["SERVER_PROTOCOL"] . ' 500 Bad Request');
+    } catch (Exception $e) {
+        header($_SERVER["SERVER_PROTOCOL"] . ' 500 Bad Request');
         die('500 Bad Request');
     }
 }, 'home');
@@ -19,10 +25,10 @@ $router->map('GET', '/[*:path]', function($path) use ($server){
 $match = $router->match();
 
 // call closure or throw 404 status
-if( $match && is_callable( $match['target'] ) ) {
-    call_user_func_array( $match['target'], $match['params'] );
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
 } else {
     // no route was matched
-    header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
     die('404 Not Found');
 }
--- server.php
+++ server.php
@@ -1,5 +1,12 @@
 <?php
 
+use League\Flysystem\Filesystem;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Glide\Manipulators;
+use League\Glide\Api\Api;
+use League\Glide\Server;
+use League\Glide\Responses;
+
 $config = require 'Config.php';
 
 // Source Client
@@ -14,38 +21,38 @@ $imageManager = new Intervention\Image\ImageManager([
 ]);
 
 // Set source filesystem
-$source = new League\Flysystem\Filesystem(
-    new \League\Flysystem\AwsS3v3\AwsS3Adapter($S3ClientSource, $config['s3']['source']['bucket'])
+$source = new Filesystem(
+    new AwsS3Adapter($S3ClientSource, $config['s3']['source']['bucket'])
 );
 
 // Set cache filesystem
-$cache = new League\Flysystem\Filesystem(
-    new \League\Flysystem\AwsS3v3\AwsS3Adapter($S3ClientSource, $config['s3']['cache']['bucket'])
+$cache = new Filesystem(
+    new AwsS3Adapter($S3ClientSource, $config['s3']['cache']['bucket'])
 );
 
 // Set manipulators
 $manipulators = [
-    new League\Glide\Manipulators\Orientation(),
-    new League\Glide\Manipulators\Crop(),
-    new League\Glide\Manipulators\Size(2000*2000),
-    new League\Glide\Manipulators\Brightness(),
-    new League\Glide\Manipulators\Contrast(),
-    new League\Glide\Manipulators\Gamma(),
-    new League\Glide\Manipulators\Sharpen(),
-    new League\Glide\Manipulators\Filter(),
-    new League\Glide\Manipulators\Blur(),
-    new League\Glide\Manipulators\Pixelate(),
-//    new League\Glide\Manipulators\Watermark($watermarks),
-    new League\Glide\Manipulators\Background(),
-    new League\Glide\Manipulators\Border(),
-    new League\Glide\Manipulators\Encode(),
+    new Manipulators\Orientation(),
+    new Manipulators\Crop(),
+    new Manipulators\Size(2000*2000),
+    new Manipulators\Brightness(),
+    new Manipulators\Contrast(),
+    new Manipulators\Gamma(),
+    new Manipulators\Sharpen(),
+    new Manipulators\Filter(),
+    new Manipulators\Blur(),
+    new Manipulators\Pixelate(),
+//    new Manipulators\Watermark($watermarks),
+    new Manipulators\Background(),
+    new Manipulators\Border(),
+    new Manipulators\Encode(),
 ];
 
 // Set API
-$api = new League\Glide\Api\Api($imageManager, $manipulators);
+$api = new Api($imageManager, $manipulators);
 
 // Setup Glide server
-$server = new League\Glide\Server(
+$server = new Server(
     $source,
     $cache,
     $api
@@ -58,6 +65,6 @@ $server->setBaseUrl($config['base_url']);
 $server->setDefaults($config['defaults']);
 
 // Set response factory
-$server->setResponseFactory(new \League\Glide\Responses\SymfonyResponseFactory());
+$server->setResponseFactory(new Responses\SymfonyResponseFactory());
 
 return $server;
